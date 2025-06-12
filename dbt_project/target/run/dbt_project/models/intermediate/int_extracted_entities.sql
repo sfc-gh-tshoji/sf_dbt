d4ecb11c48ed95184fda06779f5192e361915323
Takuya Shoji <takuya.shoji@snowflake.com>
2025-06-12 09:40:08 +0900

    Add dbt Project demo assets

---
+++ dbt_project/target/run/dbt_project/models/intermediate/int_extracted_entities.sql
@@ -0,0 +1,43 @@
+
+        
+
+    
+        create dynamic table dbt_hol_2025_prod.public_02_intermediate.int_extracted_entities
+        target_lag = 'downstream'
+        warehouse = VWH_DBT_HOL
+        refresh_mode = FULL
+
+        initialize = ON_CREATE
+
+        as (
+            
+
+with trading_books as (
+    select * from dbt_hol_2025_prod.public_01_staging.stg_trading_books
+),
+
+-- Extract sentiment using SNOWFLAKE.CORTEX.SENTIMENT
+cst as (
+    select
+        trade_id,
+        trade_date,
+        trader_name,
+        desk,
+        ticker,
+        quantity,
+        price,
+        trade_type,
+        notes,
+        SNOWFLAKE.CORTEX.SENTIMENT(notes) as sentiment,
+        SNOWFLAKE.CORTEX.EXTRACT_ANSWER(notes, 'What is the signal driving the following trade?') as signal,
+        SNOWFLAKE.CORTEX.CLASSIFY_TEXT(notes||': '|| signal[0]:"answer"::string,['Market Signal','Execution Strategy']):"label"::string as trade_driver
+    from trading_books
+    where notes is not null
+)
+select * from cst
+        )
+
+    
+
+
+    
