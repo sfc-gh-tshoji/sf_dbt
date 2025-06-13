89ecfbbf7fb3da26dcc662e983860fbc2179a7dc
sfc-gh-tshoji <takuya.shoji@snowflake.com>
2025-06-13 02:18:04 +0000

    Modified 00_satrt_here.sql and deleted .DS_Store files

--- 00_start_here.sql
+++ 00_start_here.sql
@@ -6,6 +6,30 @@
  (https://quickstarts.snowflake.com/guide/data_engineering_with_notebooks/) for all the details including set up steps.
 ***/
 
+-- Step 00 Setup Git repository
+
+show api integrations;
+
+-- Git連携のため、API統合を作成する
+-- CREATE OR REPLACE API INTEGRATION GITHUB_TSHOJI_SF_DBT_INT
+--   API_PROVIDER = git_https_api
+--   API_ALLOWED_PREFIXES = ('https://github.com/sfc-gh-tshoji/sf_dbt.git')
+--   ALLOWED_AUTHENTICATION_SECRETS = (all)
+--   ENABLED = TRUE
+--   ;
+
+show git repositories;
+show secrets;
+
+-- GITレポジトリの作成
+CREATE OR REPLACE GIT REPOSITORY sf_dbt
+  ORIGIN = 'https://github.com/sfc-gh-tshoji/sf_dbt.git'
+  API_INTEGRATION = GITHUB_TSHOJI_SF_DBT_INT
+  -- GIT_CREDENTIALS = sfc_gh_tshoji_pat
+  COMMENT = 'dbtデモ用Github'
+;
+
+
 -- Step 01 Setup dev environment
 
 USE ROLE ACCOUNTADMIN;
@@ -13,8 +37,10 @@ USE DATABASE tshoji_db;
 USE SCHEMA tshoji_schema;
 
 ALTER GIT REPOSITORY SF_DBT FETCH;
+
+ls @TSHOJI_DB.TSHOJI_SCHEMA.SF_DBT/branches/main;
 EXECUTE IMMEDIATE FROM
-    @TSHOJI_DB.TSHOJI_SCHEMA.SF_DBT/branches/main/sfguide-deploying-pipelines-with-snowflake-and-dbt-labs-main/scripts/deploy_environment.sql
+    @TSHOJI_DB.TSHOJI_SCHEMA.SF_DBT/branches/main/scripts/deploy_environment.sql
     USING (env => 'DEV')
 ;
 
@@ -25,9 +51,23 @@ USE SCHEMA tshoji_schema;
 
 ALTER GIT REPOSITORY SF_DBT FETCH;
 EXECUTE IMMEDIATE FROM
-    @TSHOJI_DB.TSHOJI_SCHEMA.SF_DBT/branches/main/sfguide-deploying-pipelines-with-snowflake-and-dbt-labs-main/scripts/deploy_environment.sql
+    @TSHOJI_DB.TSHOJI_SCHEMA.SF_DBT/branches/main/scripts/deploy_environment.sql
     USING (env => 'PROD')
 ;
+
+-- Step 03 Create TASK
+create or replace DBT PROJECT tshoji_db.tshoji_schema.dbt_quickstart 
+from snow://workspace/USER$.PUBLIC."sf_dbt"/versions/live/dbt;
+
+desc DBT PROJECT tshoji_db.tshoji_schema.dbt_quickstart;
+
+CREATE OR ALTER TASK tshoji_db.tshoji_schema.run_dbt_prod
+  WAREHOUSE = tshoji_wh
+  SCHEDULE = '1 hour'
+AS
+EXECUTE DBT PROJECT tshoji_db.tshoji_schema.dbt_quickstart_dev args='run --target prod';
+
+execute task tshoji_db.tshoji_schema.run_dbt_prod;
 
 -- Step 10 Teardown
 USE ROLE ACCOUNTADMIN;
